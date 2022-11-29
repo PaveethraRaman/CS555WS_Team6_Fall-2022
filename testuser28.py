bd59c12170b369e63afad7c3e2909bdfb984233e
DheerajReddyKadiveti <114456108+DheerajReddyKadiveti@users.noreply.github.com>
2022-11-28 22:10:07 -0500

    Delete testuser28.py

--- testuser28.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import userstory28
-
-class TestOrderSiblingsByAge(unittest.TestCase):
-    def testcase1(self):
-        self.assertTrue(userstory28.OrderSiblingsByAge != 0)
-
-unittest.main()
