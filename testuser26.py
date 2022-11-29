f074f820ad8cd2775254aac04eb9427639a94e9d
DheerajReddyKadiveti <114456108+DheerajReddyKadiveti@users.noreply.github.com>
2022-11-28 22:09:24 -0500

    Add files via upload

---
+++ testuser26.py
@@ -0,0 +1,8 @@
+import unittest
+import userstory26
+
+class TestCorrespondingEntries(unittest.TestCase):
+    def testcase1(self):
+        self.assertFalse(userstory26.CorrespondingEntries ==  0)
+
+unittest.main()
---
+++ testuser28.py
@@ -0,0 +1,8 @@
+import unittest
+import userstory28
+
+class TestOrderSiblingsByAge(unittest.TestCase):
+    def testcase1(self):
+        self.assertTrue(userstory28.OrderSiblingsByAge != 0)
+
+unittest.main()
