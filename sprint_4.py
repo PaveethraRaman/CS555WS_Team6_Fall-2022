ada409c6d5e8a2c94527f912385934e716243d8e
DheerajReddyKadiveti <114456108+DheerajReddyKadiveti@users.noreply.github.com>
2022-11-28 22:07:10 -0500

    Update sprint_4.py

--- sprint_4.py
+++ sprint_4.py
@@ -189,3 +189,45 @@ def OrderSiblingsByAge(famList, indiList):
         print("Siblings with Age in family ", family[0] )
         for i in range(len(siblings_with_age)):
             print("Name = ", getNameFromID(siblings_with_age[i][0], indiList),",","Date of Birth = ", siblings_with_age[i][1])
+	
+#userstory 29
+
+def DeceasedIndividuals(indiList):
+    length_of_indiList = len(indiList)
+    died_persons = []
+
+    for i in range(0, length_of_indiList):
+        individual = indiList[i]
+    
+        if ( individual[4] != 0):
+            died_persons.append(individual[0])
+    
+    print("Deceased Individuals are")
+    for i in range(len(died_persons)):
+        print(getNameFromID(died_persons[i], indiList))
+    
+#userstory 30 
+def LivingMarried(famList, indiList):
+    length_of_famList = len(famList)
+    length_of_indiList = len(indiList)
+
+    alive_individuals = set()
+
+
+    for i in range(0, length_of_famList):
+        family = famList[i]
+        indi1 = family[1]
+        indi2 = family[2]
+
+        person1_alive = isAlive(indi1, indiList)
+        person2_alive = isAlive(indi2, indiList)
+        
+        if (person1_alive):
+            alive_individuals.add(indi1)
+        if (person2_alive):
+            alive_individuals.add(indi2)
+    
+    print("Alive married individuals")
+    for person in alive_individuals:
+        print(getNameFromID(person, indiList))
+
