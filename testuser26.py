5e4f590fa6d37580cdfe65ab680f6b2e58155ac2
DheerajReddyKadiveti <114456108+DheerajReddyKadiveti@users.noreply.github.com>
2022-11-28 22:09:53 -0500

    Delete testuser26.py

--- testuser26.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import userstory26
-
-class TestCorrespondingEntries(unittest.TestCase):
-    def testcase1(self):
-        self.assertFalse(userstory26.CorrespondingEntries ==  0)
-
-unittest.main()
